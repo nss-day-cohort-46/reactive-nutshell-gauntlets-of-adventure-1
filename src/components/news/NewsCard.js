9023c3cc00dbb2fd55b4c0e6d462a7715fbe59d4
Erica Thompson <younique98@gmail.com>
2021-03-09 15:07:42 -0800

    fixed timeStamp that was broken due to missing onCLick and useState

--- src/components/news/NewsCard.js
+++ src/components/news/NewsCard.js
@@ -1,9 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 import "../news/News.css"
 import { Link } from "react-router-dom"
 
 
 export const NewsArticleCard = ({ newsArticle }) => {
+    let [timeSaved, setSavedTime] = useState(newsArticle.timestamp)
+
+    const handleTime = () => {
+        
+let timePosted = newsArticle.timestamp;
+const date = new Date(timePosted * 1000);
+const hours = date.getHours();
+const minutes = "0" + date.getMinutes();
+const seconds = "0" + date.getSeconds();
+const formatedTime = hours + `:` + minutes.substr(-2) + ':' + seconds.substr(-2);
+        setSavedTime(formatedTime)
+        
+}
   return (
     <section className="newsArticle">
       <h3 className="newsArticle__name">
@@ -13,5 +26,8 @@ export const NewsArticleCard = ({ newsArticle }) => {
       </h3>
       <div className="newsArticle__synopsis">{ newsArticle.synopsis }</div>
       <div className="newsArticle__url">{ newsArticle.url }</div>
+      <Link to={`${ newsArticle.url }`}></Link>
+      <div className="newsArticle__">{timeSaved}</div>
+      <button onClick={(handleTime)}>Click Me</button>
   </section>
 )}
