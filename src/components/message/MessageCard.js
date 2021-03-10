314b34228f9ad751c49c2b6bd949c3a076aa8b20
blusanders <blusanders@yahoo.com>
2021-03-10 08:19:51 -0600

    Date rendering

--- src/components/message/MessageCard.js
+++ src/components/message/MessageCard.js
@@ -9,25 +9,11 @@ export const MessageCard = ({ message }) => {
     const history = useHistory();
 
     function timeConverter(UNIX_timestamp){
-        const unixTimestamp = UNIX_timestamp
-
-        const milliseconds = UNIX_timestamp * 1000 // 1575909015000
-        
-        const dateObject = new Date(milliseconds)
-        
-        const humanDateFormat = dateObject.toLocaleString() //2019-12-9 10:30:15
-        
-        const dayText = dateObject.toLocaleString("en-US", {weekday: "short"}) // Monday
-        const month = dateObject.toLocaleString("en-US", {month: "short"}) // December
-        const day = dateObject.toLocaleString("en-US", {day: "numeric"}) // 9
-        const year = dateObject.toLocaleString("en-US", {year: "numeric"}) // 2019
-        const hour = dateObject.toLocaleString("en-US", {hour: "numeric"}) // 10 AM
-        const min = dateObject.toLocaleString("en-US", {minute: "numeric"}) // 30
-        const sec = dateObject.toLocaleString("en-US", {second: "numeric"}) // 15
-        const timeZone = dateObject.toLocaleString("en-US", {timeZoneName: "short"}) // 12/9/2019, 10:30:15 AM CST
-
-        // const time = dayText + " " + month + " " + year + " " + hour + " " + min + " " + sec + " " + timeZone
-        const time = timeZone
+        var dateVar = new Date(UNIX_timestamp).toLocaleDateString("en-US")
+        console.log(dateVar)
+        var timeVar = new Date(UNIX_timestamp).toLocaleTimeString("en-US")
+        console.log(timeVar)
+        const time = `${dateVar} ${timeVar}`
         return time;
     }
 
